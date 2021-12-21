a936a49d9740d119646347f534936033bea8ce2e
Gossteer <GossteerPlus@gmail.com>
2021-12-21 12:53:10 +0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Elena',
+            'email' => '1550371@mail.ru',
+            'password' => Hash::make('fynjy1999')
+        ]);
+    }
+}
